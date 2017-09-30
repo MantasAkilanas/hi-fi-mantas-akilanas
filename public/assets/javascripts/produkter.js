5c45c062c973ac70b298649e344c93b569efc9fd
Mantas Akilanas <4294018@RTS-365.dk>
2017-09-30 21:03:56 +0200

    updated search

--- public/assets/javascripts/produkter.js
+++ public/assets/javascripts/produkter.js
@@ -25,7 +25,7 @@
                     const div = document.createElement("DIV");
                     const h3 = document.createElement("H3");
                     var a = document.createElement("A");
-                    a.setAttribute("href", "produkter.html?kategori=" + kategori.kategori);
+                    a.setAttribute("href", "produkter.html?kategori=" + kategori.id);
                     const navn = document.createTextNode(kategori.kategori);
                     h3.appendChild(navn);
                     div.setAttribute("class", "col-xs-6 col-sm-4 col-md-3 produktDiv");
@@ -39,39 +39,42 @@
             });
     }
     const visEnKategori = function (kategori) {
-        fetch("http://localhost:1337/produkter")
+        fetch(`http://localhost:1337/produkter/${kategori}`)
             .then(function (result) {
                 return result.json();
             })
             .then(function (kategorier) {
                 const myDiv = document.querySelector(".myDiv");
-                const h2 = document.createElement("H2");
-                const h2Text = document.createTextNode(kategori);
-                h2.appendChild(h2Text);
-                myDiv.appendChild(h2);
+                var firsttime = true;
                 kategorier.forEach(function (kat) {
-                    if (kategori === kat.kategori) {
 
-                        const div = document.createElement("DIV");
-                        const billedeDiv = document.createElement("DIV");
-                        const h3 = document.createElement("H3");
-                        const img = document.createElement("img");
-                        var a = document.createElement("A");
-                        var anchor = document.createElement("A");
-
-                        a.setAttribute("href", "produkter.html?produktnr=" + kat.id);
-                        anchor.setAttribute("href", "produkter.html?produktnr=" + kat.id);
-                        const navn = document.createTextNode(kat.navn);
-                        img.setAttribute("src", "./assets/media/" + kat.billede);
-                        h3.appendChild(navn);
-                        div.setAttribute("class", "col-xs-12 col-sm-6 col-md-3 produktDiv");
-                        anchor.appendChild(img);
-                        div.appendChild(anchor);
-                        anchor.setAttribute("class", "equalImage");
-                        a.appendChild(h3);
-                        div.appendChild(a);
-                        myDiv.appendChild(div);
+                    if (firsttime) {
+                        const h2 = document.createElement("H2");
+                        const h2Text = document.createTextNode(kat.kategori);
+                        h2.appendChild(h2Text);
+                        myDiv.appendChild(h2);
+                        firsttime = false;
                     }
+
+                    const div = document.createElement("DIV");
+                    const billedeDiv = document.createElement("DIV");
+                    const h3 = document.createElement("H3");
+                    const img = document.createElement("img");
+                    var a = document.createElement("A");
+                    var anchor = document.createElement("A");
+
+                    a.setAttribute("href", "produkter.html?produktnr=" + kat.id);
+                    anchor.setAttribute("href", "produkter.html?produktnr=" + kat.id);
+                    const navn = document.createTextNode(kat.navn);
+                    img.setAttribute("src", "./assets/media/" + kat.billede);
+                    h3.appendChild(navn);
+                    div.setAttribute("class", "col-xs-12 col-sm-6 col-md-3 produktDiv");
+                    anchor.appendChild(img);
+                    div.appendChild(anchor);
+                    anchor.setAttribute("class", "equalImage");
+                    a.appendChild(h3);
+                    div.appendChild(a);
+                    myDiv.appendChild(div);
 
                 })
                 setTimeout(function () { equalColumns(); }, 30);
@@ -80,7 +83,7 @@
 
     }
     const visEnProdukt = function (produkt) {
-        fetch("http://localhost:1337/produkt")
+        fetch(`http://localhost:1337/produkt/${produkt}`)
             .then(function (result) {
                 return result.json();
             })
@@ -102,9 +105,7 @@
                         var div = document.createElement("DIV");
                         div.setAttribute("class", "col-xs-12 col-md-6 produktDiv");
                         Object.keys(prod).forEach(function (key) {
-
                             if (key !== "billede") {
-                                console.log(key);
                                 var paragraph = document.createElement("P");
                                 var value = document.createTextNode(key);
                                 paragraph.setAttribute("class", "col-xs-6")
@@ -125,27 +126,20 @@
             });
     }
     const findProdukt = function (find) {
-        fetch("http://localhost:1337/produkter")
+        fetch(`http://localhost:1337/search/${find}`)
             .then(function (result) {
                 return result.json();
             })
             .then(function (fundet) {
                 fundet.forEach(function (fund) {
                     const myDiv = document.querySelector(".myDiv");
-                    if (find == fund.id || find == fund.navn || find == fund.kategori || find == fund.producent) {
-
-                        
-                        const p = document.createElement("P");
-                        const pText = document.createTextNode(fund.navn);
-                        const a = document.createElement("A");
-                        a.setAttribute("href","produkter.html?produktnr=" +fund.id);
-                        p.appendChild(pText);
-                        a.appendChild(p);
-                        myDiv.appendChild(a);
-
-
-
-                    }
+                    const p = document.createElement("P");
+                    const pText = document.createTextNode(fund.navn);
+                    const a = document.createElement("A");
+                    a.setAttribute("href", "produkter.html?produktnr=" + fund.id);
+                    p.appendChild(pText);
+                    a.appendChild(p);
+                    myDiv.appendChild(a);
                 })
             });
     }
